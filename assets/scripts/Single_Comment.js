7b4d7bd72f5288fb106bb8a171a5e3a6d6ce5c04
Jacwa Lei <jalei@ucsd.edu>
2022-11-21 05:26:32 -0800

    added localstorage and single comment stuff

---
+++ assets/scripts/Single_Comment.js
@@ -0,0 +1,99 @@
+// Single_Comment.js
+
+// This exists as a placeholder atm to render the comments/feedback
+class SingleComment extends HTMLElement {
+    constructor() {
+      super(); // Inherit everything from HTMLElement
+      //Attach the shadow DOM to this Web Component (leave the mode open)
+      const shadow = this.attachShadow({mode: 'open'});
+      //Create an <article> element - This will hold our markup once our data is set
+      const newArticle = document.createElement('article');
+      //Create a style element - This will hold all of the styles for the Web Component
+      const newStyle = document.createElement('style');
+      //Insert all of the styles from cardTemplate.html into the <style> element you just made
+      newStyle.textContent = `
+      * {
+        font-family: sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+    
+      article {
+        align-items: center;
+        border: 1px solid rgb(223, 225, 229);
+        border-radius: 8px;
+        display: grid;
+        grid-template-rows: 118px 56px 14px 18px 15px 36px;
+        height: auto;
+        row-gap: 5px;
+        padding: 0 16px 16px 16px;
+        width: 178px;
+      }
+    
+      p.title {
+        display: -webkit-box;
+        font-size: 16px;
+        height: 36px;
+        line-height: 18px;
+        overflow: hidden;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+    
+      p.category {
+        font-size: 10px;
+      }
+
+      p:not(.title),
+      span {
+        color: #70757A;
+        font-size: 12px;
+      }`
+      
+      shadow.appendChild(newStyle);
+      shadow.appendChild(newArticle);
+    }
+  
+    /**
+     * Called when the .data property is set on this element.
+     *
+     *
+     *
+     * @param {Object} data - The data to pass into must be of the
+     *                        following format:
+     *                        {
+     *                          "title": "string",
+     *                          "classname": "string",
+     *                          "category": "string",
+     *                          "date": "string",
+     *                          "feedBack": "textarea",
+     *                        }
+     */
+    set data(data) {
+      // If nothing was passed in, return
+      if (!data) return;
+  
+      console.log(data);
+      const addedArticle = this.shadowRoot.querySelector("article");
+      
+      console.log(data.classname);
+      addedArticle.innerHTML=`
+        <p class="title">`+data.title+`</p>
+        <p class="classname">`+data.classname+`</p>
+        <div class="date">
+          <span>(`+data.date+`)</span>
+        </div>
+        <p class="feedback_content">`
+          +data.feedBack+
+        `</p>
+        <p class="category">`
+          +data.category+
+        `</p>
+        <button type="button" class="update">Update Feedback</button>
+        <button type="button" class="delete">Delete Feedback</button>
+      `; // would eventually prefer to change to an icon button or something
+    }
+  }
+  
+
+  customElements.get('the-element')||customElements.define("the-element",SingleComment);
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,23 +7,18 @@ function init() {
     let saveButton = document.getElementById('saveButton'); // this button is used inside the dialog box
     let dialog = document.querySelector('dialog'); // this element is the dialog box itself
     let cancelButton = document.getElementById('cancelButton'); // this button is used inside the dialog box
-    let firstRow = null; // this is the first row of the table
-    let currentRow = null; // this is the row that is currently selected and is under work 
+    
+    // remove these
+    // let firstRow = null; // this is the first row of the table
+    // let currentRow = null; // this is the row that is currently selected and is under work 
+    
+    
     let viewFeedbackButton = document.getElementById('viewFeedback'); // element for view feedback button show trigger the feedbacks dialog box to open
     let viewFeedbackDialog = document.getElementById('feedbackListTable'); //this dialog box will be triggered by view feedbacks button and
-    let closeFeedbackDialog = document.getElementById('closeButton'); // close the feedbacks dialog box
+    //let closeFeedbackDialog = document.getElementById('closeButton'); // close the feedbacks dialog box
     let confirmationMessage = document.getElementById('confirmationMessage'); // just a confirmation meessage to assure the user that the input has been saved. the feedback can be seen by clicking view feedback button
     let viewTableDialog = document.getElementById('viewTable'); // just a confirmation meessage to assure the user that the input has been saved. the feedback can be seen by clicking view feedback button
     let viewTableCloseButton = document.getElementById('viewCloseB');
-
-    
-    // create view table
-    var table = document.getElementById("feedbackList").getElementsByTagName('tbody')[0];
-    var viewFeedbackTable = document.getElementById("viewList").getElementsByTagName('tbody')[0];
-    let newViewRow = viewFeedbackTable.insertRow(-1);
-    let viewcell1 = newViewRow.insertCell(0);
-    let newViewRow2 = viewFeedbackTable.insertRow(-1);
-    let viewcell2 = newViewRow2.insertCell(0);
 
     //when the newFeedbackButton is clicked, th dialog box should open
     newFeedbackButton.addEventListener('click', () => {
@@ -36,16 +31,71 @@ function init() {
     cancelButton.addEventListener('click', () => {
         dialog.close();
     });
-
    
-    //when save button is clicked, the class name, date and feedback needs to be saved and ready to be shown
+    // when save button is clicked, the class name, date and feedback needs to be saved and ready to be shown
     saveButton.addEventListener('click', () => {
+        let dialogObj = createFeedbackObject();
+        
+        const curcomments = getCommentsFromStorage();
+        curcomments.push(dialogObj);
+        saveCommentToStorage(curcomments);
+
+        // if (firstRow == null) {
+        //     let newRow = table.insertRow(-1);
+        //     let cell1 = newRow.insertCell(0);
+        //     cell1.innerHTML = dialogInput.className;
+        //     let cell2 = newRow.insertCell(1);
+        //     cell2.innerHTML = dialogInput.date;
+        //     let cell3 = newRow.insertCell(2);
+        //     cell3.innerHTML = dialogInput.category;
+        //     let cell4 = newRow.insertCell(3);
+
+        //     let viewPage = document.createElement("button");
+        //     viewPage.innerHTML = "View Feedback";
+        //     //when the deleteButton is clicked, the row should be deleted
+        //     viewPage.onclick = function(){
+        //         if (typeof viewTable.showModal === "function") { // check if the dialog is already open or not
+        //             viewTableDialog.showModal(); // open the dialog box
+        //         }
+        //         let rowIdx = viewPage.parentNode.parentNode.rowIndex;
+        //         viewcell1.innerHTML = dialogInput.feedBack;
+        //         viewcell2.innerHTML = table.rows[rowIdx-1].cells[3].innerHTML;;
+        //     };
+        //     cell4.appendChild(viewPage);
+
+        //     let cell5 = newRow.insertCell(4);
+        //     let deleteButton = document.createElement("button");
+        //     deleteButton.innerHTML = "Delete Feedback"
+        //     //when the deleteButton is clicked, the row should be deleted
+        //     deleteButton.onclick = function(){
+        //         deleteButton.parentNode.parentNode.remove();
+        //     };
+        //     cell5.appendChild(deleteButton);
+
+        // } else {
+        //     currentRow.cells[0].innerHTML = dialogInput.className;;
+        //     currentRow.cells[1].innerHTML = dialogInput.date;
+        //     currentRow.cells[2].innerHTML = dialogInput.category;
+        //     currentRow.cells[3].innerHTML = dialogInput.feedBack;
+        // }
+        resetForm();
+        console.log("saved");
+        confirmationMessage.textContent = "Feedback saved!";
+    });
+
+    /// maybe formalize? createFeedbackObject as actual function with header
+
+    /// creates a comment/feedback object
+    let createFeedbackObject = () => {
+        // get the input data from the dialog
         let dialogInput = {};
-        dialogInput.className = document.getElementById("title")?.value;
+        dialogInput.title = document.getElementById("title")?.value;
+        dialogInput.className = document.getElementById("className")?.value;
         dialogInput.date = document.getElementById("date")?.value;
         dialogInput.category = document.getElementById("category")?.name;
         dialogInput.feedBack = document.querySelector("textarea")?.value;
 
+        // there might be a cleaner way to do this with just the comment storage
         // check if class exists in local storage
         if(localStorage.classList==undefined){localStorage.setItem("classList",JSON.stringify([]));} 
         // maybe move this somewhere where it doesnt need to be run repeatedly
@@ -55,76 +105,59 @@ function init() {
             classList?.push(dialogInput.className);
             localStorage.setItem("classList", JSON.stringify(classList));
         }
-        
-
-        if (firstRow == null) {
-            let newRow = table.insertRow(-1);
-            let cell1 = newRow.insertCell(0);
-            cell1.innerHTML = dialogInput.className;
-            let cell2 = newRow.insertCell(1);
-            cell2.innerHTML = dialogInput.date;
-            let cell3 = newRow.insertCell(2);
-            cell3.innerHTML = dialogInput.category;
-            let cell4 = newRow.insertCell(3);
-
-            let viewPage = document.createElement("button");
-            viewPage.innerHTML = "View Feedback";
-            //when the deleteButton is clicked, the row should be deleted
-            viewPage.onclick = function(){
-                if (typeof viewTable.showModal === "function") { // check if the dialog is already open or not
-                    viewTableDialog.showModal(); // open the dialog box
-                }
-                let rowIdx = viewPage.parentNode.parentNode.rowIndex;
-                viewcell1.innerHTML = dialogInput.feedBack;
-                viewcell2.innerHTML = table.rows[rowIdx-1].cells[3].innerHTML;;
-            };
-            cell4.appendChild(viewPage);
-
-            let cell5 = newRow.insertCell(4);
-            let deleteButton = document.createElement("button");
-            deleteButton.innerHTML = "Delete Feedback"
-            //when the deleteButton is clicked, the row should be deleted
-            deleteButton.onclick = function(){
-                deleteButton.parentNode.parentNode.remove();
-            };
-            cell5.appendChild(deleteButton);
-
-        } else {
-            currentRow.cells[0].innerHTML = dialogInput.className;;
-            currentRow.cells[1].innerHTML = dialogInput.date;
-            currentRow.cells[2].innerHTML = dialogInput.category;
-            currentRow.cells[3].innerHTML = dialogInput.feedBack;
-        }
-        resetForm();
-        console.log("saved");
-        confirmationMessage.textContent = "Feedback saved!";
-    });
+        return dialogInput;
+    }
 
     // after each save button click, the form needs to be cleared so for next new feedback it will be ready
     let resetForm = () => {
         document.getElementById("title").value = "";
+        document.getElementById("className").value = "";
         document.getElementById("date").value = "";
-        document.getElementById("category").value = "";
         document.querySelector("textarea").value = "";
-        currentRow = null;
+        // reset all radio buttons
+        let radio = document.getElementsByName("category");
+        for (let i=0; i < radio.length; i++){
+            radio[i].checked = false;
+        }
+    }
+
+    /**
+    * Reads 'comments' from localStorage and returns an array of
+    * all of the comments found (parsed, not in string form). If
+    * nothing is found in localStorage for 'comments', an empty array
+    * is returned.
+    * @returns {Array<Object>} An array of recipes found in localStorage
+    */
+     function getCommentsFromStorage() {
+        if (localStorage.getItem('comment') == null) {
+            const emptyArray = [];
+            return emptyArray;
+        }
+        const str = localStorage.getItem("comment");
+        return JSON.parse(str);
+    }
+
+    function saveCommentToStorage(comment) {    
+        localStorage.setItem("comment",JSON.stringify(comment));
     }
 
     // this will trigger the dialog box that has all the feedbacks so the user can see them
     viewFeedbackButton.addEventListener('click', () => {
-        if (typeof viewFeedbackDialog.showModal === "function") { // check if the dialog is already open or not
-            viewFeedbackDialog.showModal(); // open the dialog box
-        }
+        // TODO: use Single_Comment.js to generate viewable comments
+        // if (typeof viewFeedbackDialog.showModal === "function") { // check if the dialog is already open or not
+        //     viewFeedbackDialog.showModal(); // open the dialog box
+        // }
     });
 
-    // this will close the dialog box that has the feedbacks list
-    closeFeedbackDialog.addEventListener('click', () => {
-        viewFeedbackDialog.close();
-    });
+    // // this will close the dialog box that has the feedbacks list
+    // closeFeedbackDialog.addEventListener('click', () => {
+    //     viewFeedbackDialog.close();
+    // });
 
-    // this will close the dialog box that has the view list
-    viewTableCloseButton.addEventListener('click', () => {
-        viewTableDialog.close();
-    });
+    // // this will close the dialog box that has the view list
+    // viewTableCloseButton.addEventListener('click', () => {
+    //     viewTableDialog.close();
+    // });
 
     
    
