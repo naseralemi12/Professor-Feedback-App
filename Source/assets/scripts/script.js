90b99dee5100c2ab4c238102d522bd2eef9ec78c
Chris <Chrischcc@outlook.com>
2022-11-21 13:58:12 -0800

    Complete functionalities professor_modify_categroy

--- Source/assets/scripts/script.js
+++ Source/assets/scripts/script.js
@@ -16,7 +16,50 @@ function init() {
     let viewTableDialog = document.getElementById('viewTable'); // just a confirmation meessage to assure the user that the input has been saved. the feedback can be seen by clicking view feedback button
     let viewTableCloseButton = document.getElementById('viewCloseB');
 
-    
+
+    let TestclassList = ["CSE110","CSE101"];
+    localStorage.setItem("ClassList",JSON.stringify(TestclassList));
+    //Initialize all exist classes' category
+    let classList = JSON.parse(localStorage.getItem("ClassList"));
+    for(let i=0;i<classList?.length;i++){
+        NewclassCategory(classList[i]);
+    }
+
+    //This part is for professor_modify_category
+    const categoryTable = document.getElementById("categorylist");
+    var list = JSON.parse(localStorage.getItem("CSE110"));
+    for(let i=0;i<list.length;i++){
+        var newRow = categoryTable.insertRow();
+        var newCell = newRow.insertCell();
+        var newCategory =document.createTextNode(list[i]);
+        newCell.appendChild(newCategory);
+        //category list for given class
+        let cell = newRow.insertCell(1);
+        let deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete Category";
+        //when the deleteButton is clicked, the row should be deleted
+        deleteButton.onclick = function(){
+            deleteButton.parentNode.parentNode.remove();
+            deleteCategory("CSE110",list[i]);
+        };
+        cell.appendChild(deleteButton);
+    }
+
+    /*TEST for professor_modify_category
+    const categoryTable = document.getElementById("categorylist");
+    for(let i=0;i<2;i++){
+        var newRow = categoryTable.insertRow();
+        var newCell = newRow.insertCell();
+        var newCategory = document.createTextNode('new row');
+        newCell.appendChild(newCategory);
+        //category list for given class
+        let cell = newRow.insertCell(1);
+        let deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete Category"
+        cell.appendChild(deleteButton);
+    }
+    */
+
     // create view table
     var table = document.getElementById("feedbackList").getElementsByTagName('tbody')[0];
     var viewFeedbackTable = document.getElementById("viewList").getElementsByTagName('tbody')[0];
@@ -115,6 +158,20 @@ function init() {
         viewTableDialog.close();
     });
 
-    
-   
+}
+
+
+function NewclassCategory(className){
+    var category = ["Exam","Lecture","Discussion"];
+    localStorage.setItem(className,JSON.stringify(category));
+}
+
+function deleteCategory(classname,category){
+    var currcategory = JSON.parse(localStorage.getItem(classname));
+    console.log(currcategory);
+    currcategory = currcategory.filter(function(item) {
+        return item !== category
+    })
+    localStorage.setItem(classname,JSON.stringify(currcategory));
+    console.log(currcategory);
 }
