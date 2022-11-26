328596d806e5855d6cebec7503228bcb27206f09
Christian Velasquez <cjvelasq@ucsd.edu>
2022-11-26 09:39:43 -0800

    changes

--- assets/scripts/view_fb.js
+++ assets/scripts/view_fb.js
@@ -1,7 +1,17 @@
 window.addEventListener('DOMContentLoaded', init);
-
+import { filterSubmissions } from "./helpers.js"
 function init() {
     loadComments();
+
+    // attach event listener to all comments
+    let comments = document.querySelectorAll('.expandInfo');
+    for (let item in comments) {
+        comments[item].addEventListener('click', () => {
+            document.querySelector('#commentTitle').innerHTML= comments[item].innerText; // setting title
+            let submissionsArr = filterSubmissions(JSON.parse(localStorage.getItem("submissions")));
+            document.querySelector('.textbox').innerText = submissionsArr[item].feedBack;
+        });
+    }
 }
 
 /**
@@ -9,15 +19,16 @@ function init() {
 *
 * @authors Kenny 
 */
- function loadComments() {
-    let submissionsArr = JSON.parse(localStorage.getItem("submissions"))
+function loadComments() {
+    let submissionsArr = filterSubmissions(JSON.parse(localStorage.getItem("submissions")));
     for (let i = 0; i < submissionsArr.length; i++) {
         const div = document.createElement('div');
-        let contentText = submissionsArr[i].feedBack;
+        let contentText = submissionsArr[i].title;
         div.className = 'commentBox';
         div.innerHTML = `
-            <p>${contentText}</p>
+            <a class="expandInfo">${contentText}</a>
         `;
         document.querySelector('.commentLinks').appendChild(div);
     }
 }
+
