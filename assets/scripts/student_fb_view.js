03bc6faf700706d3fff1065348697257ffae4b92
Kenny Fong <kefong@ucsd.edu>
2022-11-30 01:06:18 -0800

    finished student_fb_view styling

--- assets/scripts/student_fb_view.js
+++ assets/scripts/student_fb_view.js
@@ -1,48 +1,55 @@
+window.addEventListener('DOMContentLoaded', init);
+
 /**
- * student_fb_view.js
+ * function init()
  * 
  * Functionality for the student feedback view page
  * Has a dropdown list and two buttons to view and add feedback.
  * @author Christian Velasquez, Kenny Fong
  */
-
-window.addEventListener('DOMContentLoaded', init);
-var selectedClass;
 function init() {
-    const dropdownList = document.getElementById("classSelect");
-    // necessary function definition ? 
+    const drop_down_list = document.getElementById("classSelect");
+    /**
+    * function generateDropDown()
+    * 
+    * Functionality for creating the dropdown list
+    * @author Christian Velasquez, Kenny Fong
+    */
     function generateDropDown() {
-        dropdownList.hidden = false;
-        dropdownList.innerHTML = '<option value = "">See All</option>';
-        for (let i = 0; i < classList.length; i++) {
+        drop_down_list.hidden = false;
+        drop_down_list.innerHTML = '<option value = "">See All</option>';
+        // create dropdown list
+        for (let i = 0; i < class_list.length; i++) {
             const temp = document.createElement('option');
-            temp.value = classList[i];
-            temp.innerHTML = classList[i];
-            dropdownList?.append(temp);
+            temp.value = class_list[i];
+            temp.innerHTML = class_list[i];
+            drop_down_list?.append(temp);
         }
     }
     // Creates classList if it doesn't already exist
-    if (localStorage.classList == undefined) { localStorage.setItem("classList", JSON.stringify([])); }
-    const classList = JSON.parse(localStorage.classList);
+    if (localStorage.classList == undefined) {
+        localStorage.setItem("classList", JSON.stringify([]));
+    }
+    const class_list = JSON.parse(localStorage.classList);
 
 
     // generate the dropdown selection
     generateDropDown();
-    let fbButton = document.getElementById('addComment');
-    let viewFb = document.getElementById('view');
-    // take whatever dropdownList.value is set to at the time of click & set local currClass to it
-    fbButton.addEventListener('click' , () => {
+    let fb_button = document.getElementById('addComment');
+    let view_fb = document.getElementById('view');
+    // take whatever drop_down_list.value is set to at the time of click & set local curr_class to it
+    fb_button.addEventListener('click' , () => {
         // replace held value if different
-        let currClass = {
+        let curr_class = {
             class : document.getElementById("classSelect").value,
         }
-        localStorage.setItem('currClass', JSON.stringify(currClass));
+        localStorage.setItem('currClass', JSON.stringify(curr_class));
     });
-    viewFb.addEventListener('click' , () => {
+    view_fb.addEventListener('click' , () => {
         // replace held value if different
-        let currClass = {
+        let curr_class = {
             class : document.getElementById("classSelect").value,
         }
-        localStorage.setItem('currClass', JSON.stringify(currClass));
+        localStorage.setItem('currClass', JSON.stringify(curr_class));
     });
 }
