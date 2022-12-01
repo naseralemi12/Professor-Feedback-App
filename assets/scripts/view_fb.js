3715fd357db0e7ceb8280509c1758abc2c65ad5f
Jacwa Lei <jalei@ucsd.edu>
2022-11-30 20:59:12 -0800

    added filtering by category

--- assets/scripts/view_fb.js
+++ assets/scripts/view_fb.js
@@ -3,10 +3,7 @@ import { filterSubmissions } from "./helpers.js"
 function init() {
     loadComments();
     let categorySelection = document.getElementById("categorySelect");
-
-    categorySelection?.addEventListener("change", () =>{
-        loadComments();
-    });
+    
     let currClass = JSON.parse(localStorage.currClass).class;
     let classCategories = JSON.parse(localStorage.getItem(currClass));
     for (let i = 0; i < classCategories.length; i++) {
@@ -15,40 +12,66 @@ function init() {
         temp.innerHTML = classCategories[i];
         categorySelection?.append(temp);
     }
-    // attach event listener to all comments
-    let comments = document.querySelectorAll('.expandInfo');
-    for (let i = 0; i < comments.length; i++) {
-        comments[i].addEventListener('click', () => {
-            document.querySelector('#commentTitle').innerHTML = "Title: " + comments[i].innerText; // setting title
-            let submissionsArr = filterSubmissions(JSON.parse(localStorage.getItem("submissions")));
-            document.querySelector('.textbox').innerText = submissionsArr[i].feedBack;
-            document.querySelector('#Categoryname').innerText = "Category: " + submissionsArr[i].category;
-            document.querySelector('#Date').innerText = "Date: " + submissionsArr[i].date;
-            document.querySelector('#Name').innerText = "Name: " + submissionsArr[i].name;
-        });
-    }
-    
+
+    categorySelection?.addEventListener("change", () =>{
+        loadComments();
+        // attach event listener to all comments
+        let comments = document.querySelectorAll('.commentBox'); // changed from expandinfo to commentbox
+        for (let i = 0; i < comments.length; i++) {
+            comments[i].addEventListener('click', () => {
+                let display = document.getElementById("feedbackDisplay");
+                display.hidden = false;
+
+                document.querySelector('#commentTitle').innerHTML = "Title: " + comments[i].innerText; // setting title
+                let submissionsArr = filterSubmissions(JSON.parse(localStorage.getItem("submissions")));
+                document.querySelector('.textbox').innerText = submissionsArr[i].feedBack;
+                document.querySelector('#Categoryname').innerText = "Category: " + submissionsArr[i].category;
+                document.querySelector('#Date').innerText = "Date: " + submissionsArr[i].date;
+                document.querySelector('#Name').innerText = "Name: " + submissionsArr[i].name;
+            });
+        }     
+    });
+
     /**
-* Populates the page with comments
-*
-* @authors Kenny 
-*/
+    * Populates the page with comments
+    *
+    * @authors Kenny 
+    */
     function loadComments() {
+        let categorySelection = document.getElementById("categorySelect");
+        
+        // TODO: get see all to work
+        // if (categorySelection.value == ""){
+        //     let submissionsArr = JSON.parse(localStorage.getItem("submissions"));
+        //     document.querySelector('.commentLinks').innerHTML = "";
+        //     for (let i = 0; i < submissionsArr.length; i++) {
+        //         const div = document.createElement('div');
+        //         let contentText = submissionsArr[i].title;
+        //         div.className = 'commentBox';
+        //         div.innerHTML = `
+        //             <a class="expandInfo">${contentText}</a>
+        //         `;
+        //         document.querySelector('.commentLinks').appendChild(div);
+        //     }
+        //     return;   
+        // }
+        
         let submissionsArr = filterSubmissions(JSON.parse(localStorage.getItem("submissions")));
         let temp = [];
         for (const arrIdx in submissionsArr) {
-            if (submissionsArr[arrIdx].category ==  categorySelection.value) {
-            filteredArray.push(submissionsArr[arrIdx]);
+            if (submissionsArr[arrIdx].category == categorySelection.value) {
+            temp.push(submissionsArr[arrIdx]);
             }
         }
-        return filteredArray;
+        submissionsArr = temp;
+        document.querySelector('.commentLinks').innerHTML = "";
         for (let i = 0; i < submissionsArr.length; i++) {
             const div = document.createElement('div');
             let contentText = submissionsArr[i].title;
             div.className = 'commentBox';
             div.innerHTML = `
-            <a class="expandInfo">${contentText}</a>
-        `;
+                <a class="expandInfo">${contentText}</a>
+            `;
             document.querySelector('.commentLinks').appendChild(div);
         }
         
